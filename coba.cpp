1a81126295cad0d5e1ab2b0ce40c14cbfe5456fd
kyy08 <kyyelzz1@gmail.com>
2023-03-06 11:52:39 +0700

    first commit

---
+++ coba.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout  << "reza anak baik";
+
+     return 0;
+}
---
+++ latihan1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    // Begin
+    // Numeric Luas,p,l
+    double Luas,p,l;
+    // display 'Masukan Panjangnya = '
+    cout << "Masukan Panjangnya =";
+    // Accept p
+    cin >> p;
+    // display 'Masukan Lebarnya = ';
+    cout << "Masukan Lebarnya = ";
+    // Accept
+    cin >> l;
+    // Compute Luas = p*l
+    Luas = p*l;
+    // display "luasnya = "+Luas
+    cout << "Luasnya = " << Luas;
+    // End
+}
